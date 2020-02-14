85dc3625f88cf5b5ed1f14e87598f89f83479d9a
Jasonyang1991 <692244840@qq.com>
2020-02-14 13:01:24 +0100

    xxxx

--- public.bat
+++ public.bat
@@ -1,1 +1,5 @@
-hugo --theme=hugo-tranquilpeak-theme --baseUrl="https://jasonyang1991.github.io/"
+git add -A
+git commit -m "xxxx"
+git push -u origin master
+
+git remote add origin git@github.com:Jasonyang1991/Jasonyang1991.github.io.git
